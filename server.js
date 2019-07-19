80c71b620d97b7b59ccfdfc8861130974034c9fd
Jettapat <hennet2@hotmail.com>
2019-07-19 16:21:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ var port = process.env.PORT || 8080;
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
+app.use(allowCrossDomain);
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
