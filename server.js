90aa9e1a4ea8629a9f29d99a67ab5791eaef64b1
Jettapat <hennet2@hotmail.com>
2019-07-19 16:17:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 
 app.get('/', db.getProd);
 app.get('/product3/:id', db.getProdById3);
