5fad214128db3d880bc76f209234d7bd1bbc812e
Jettapat <hennet2@hotmail.com>
2019-10-31 20:09:04 +0700

    Update queries.js

--- queries.js
+++ queries.js
@@ -12,26 +12,19 @@ const pool = new Pool({
 })
 
 
-//QRCODE
-const getProdById3 = (request, response) => {
-  const id = request.params.id
-  let date_ob = new Date();
-  var day = date_ob.getDate();
-  var month = ((date_ob.getMonth() + 1));
-  //pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
-  
-  pool.query('SELECT * FROM public_b1.retail_comp AS RC INNER JOIN (SELECT itemcode,upsale FROM  public_b1.item_margin WHERE day=' + day + 'and month=' + month + '  ) DT ON DT.ItemCode=RC.item_id  WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
-    //pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1;', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
-}
+const getProd = (request, response) => {
+    pool.query('SELECT * FROM  public_b1.retail_comp;', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
 
 //get picture
 const getPic = (request, response) => {
-    pool.query('SELECT item_code , new_name , item_pic FROM public_b1.item_offer;', (error, results) => {
+   //pool.query('SELECT item_code , new_name , item_pic FROM public_b1.item_offer;', (error, results) => {
+  pool.query('SELECT * FROM public_b1.retail_comp AS RC INNER JOIN (SELECT itemcode,upsale FROM  public_b1.item_margin WHERE day=' + day + 'and month=' + month + '  ) DT ON DT.ItemCode=RC.item_id  WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC limit 1;', [id], (error, results) => {
       if (error) {
         throw error
       }
@@ -53,6 +46,16 @@ const getPic = (request, response) => {
   }
 
 
+  const getProdById3 = (request, response) => {
+    const id = request.params.id
+    pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp where item_id = $1) ORDER BY price ASC limit 1;', [id], (error, results) => {
+//pool.query('SELECT * FROM public_b1.retail_comp WHERE item_id = $1 and timestamp IN (SELECT max(timestamp) FROM public_b1.retail_comp) ORDER BY price ASC;', [id], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
   
   
   const getProdById4 = (request, response) => {
